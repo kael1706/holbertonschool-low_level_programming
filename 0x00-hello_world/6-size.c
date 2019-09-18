e35a0e938ae9a3addf9c6b31de2f64a5f815b73d
kael <cdcp-1795@hotmail.com>
2019-09-18 23:19:25 +0000

    fix answer 16

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,12 +14,12 @@ long long d;
 float e;
 
 /* lu es para enteros o largos indefinidos*/
-printf("Size of a char: %lu 1 byte(s)", sizeof(a));
-printf("Size of an int: %lu 4 byte(s)", sizeof(b));
+printf("Size of a char: %lu 1 byte(s)\n", sizeof(a));
+printf("Size of an int: %lu 4 byte(s)\n", sizeof(b));
 
-printf("Size of a long int: %lu 8 byte(s)", sizeof(c));
-printf("Size of a long long int: %lu 8 byte(s)", sizeof(d));
+printf("Size of a long int: %lu 8 byte(s)\n", sizeof(c));
+printf("Size of a long long int: %lu 8 byte(s)\n", sizeof(d));
 
-printf("Size of a float: %lu 4 byte(s)", sizeof(e));
+printf("Size of a float: %lu 4 byte(s)\n", sizeof(e));
 return (0);
 }
