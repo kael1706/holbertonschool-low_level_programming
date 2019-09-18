0a59b69d241700160be466819364a9075fb6a343
kael <cdcp-1795@hotmail.com>
2019-09-18 23:11:11 +0000

    fix answer 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-char a='';
-int b='';
-long c='';
-long long d='';
-float e='';
 int main(void)
 {
+char a;
+int b;
+long c;
+long long d;
+float e;
+
 /* lu es para enteros o largos indefinidos*/
 printf("Size of a char: %lu 1 byte(s)", sizeof(a));
 printf("Size of an int: %lu 4 byte(s)", sizeof(b));
