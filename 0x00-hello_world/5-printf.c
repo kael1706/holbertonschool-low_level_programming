64eb1092b589d9a87a7b84521797de5c56b05f0a
kael <cdcp-1795@hotmail.com>
2019-09-18 22:13:47 +0000

    answer 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char msj1[] = "with proper grammar, but the outcome is a piece of art,\n";
+printf("%s", msj1);
+return (0);
+}
