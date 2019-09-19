aa3de52d4bc89ea3458e222ba5ee056fc2d8e030
kael <cdcp-1795@hotmail.com>
2019-09-19 22:14:45 +0000

    answer 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *   main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter;
+for (letter = 'a' ; letter <= 'z' ; letter++)
+{
+putchar(letter);
+}
+putchar ("\n");
+return (0);
+}
