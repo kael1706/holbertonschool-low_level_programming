5697c2457135976aef7686564b171e08db21c973
kael <cdcp-1795@hotmail.com>
2019-09-19 04:37:46 +0000

    fix answer 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,20 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char TemporalMsg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+/*
+char TemporalMsg[] = and that piece of art is useful\" - Dor
+a Korpar, 2015-10-19;
 setenv("GlobalTemporalMsg", TemporalMsg, 1);
 system("echo $GlobalTemporalMsg");
+*/
+iSaid();
+return (1);
+}
+iSaid(void){
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
 return (1);
 }
