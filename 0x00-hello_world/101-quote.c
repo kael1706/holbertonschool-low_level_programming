80925b01f6083e486e5b4854ac51380e2618be52
kael <cdcp-1795@hotmail.com>
2019-09-19 04:55:17 +0000

    fix 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,6 @@
  */
 int main(void)
 {
-iSaid();
-return (1);
-}
-iSaid(void){
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 63);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 return (1);
 }
