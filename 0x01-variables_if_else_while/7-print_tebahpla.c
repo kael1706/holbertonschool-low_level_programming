e6ae87b9c8e020b6d637197b1b601eb630aea304
kael <cdcp-1795@hotmail.com>
2019-09-19 23:05:40 +0000

    answer 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *   main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letters;
+for (letters = 'z' ; letters >= 'a' ; letters--)
+{
+putchar(letters);
+}
+putchar ('\n');
+return (0);
+}
