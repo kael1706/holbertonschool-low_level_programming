030759031ad5c58bb5fd8e3fcb38bd14b7320743
kael <cdcp-1795@hotmail.com>
2019-09-19 23:13:28 +0000

    fix answer 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *   main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letters;
+for (letters = '0' ; letters <= '9' ; letters++)
+{
+putchar(letters);
+}
+for (letters = 'a' ; letters <= 'z' ; letters++)
+{
+putchar(letters);
+}
+putchar ('\n');
+return (0);
+}
