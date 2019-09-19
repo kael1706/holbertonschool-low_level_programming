834473b5a12d7f4960e94890fe1ce4a58671e094
kael <cdcp-1795@hotmail.com>
2019-09-19 04:12:56 +0000

    answer 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+char TemporalMsg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+setenv("GlobalTemporalMsg", TemporalMsg, 1);
+system("echo $GlobalTemporalMsg");
+return (1);
+}
