7139cb870433666265f5bf5c501dbef6591416ac
kael <cdcp-1795@hotmail.com>
2019-09-19 22:17:27 +0000

    fix answer 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 /**
  *   main - Entry point
  *
  *  Return: Always 0 (Success)
  */
-
 int main(void)
 {
 char letter;
