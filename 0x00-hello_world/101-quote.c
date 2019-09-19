89583416f4d8cf0488655f0e89ab1db885661d99
kael <cdcp-1795@hotmail.com>
2019-09-19 13:18:18 +0000

    fix answer 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-char TemporalMsg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-setenv("GlobalTemporalMsg", TemporalMsg, 1);
-system("echo $GlobalTemporalMsg");
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
