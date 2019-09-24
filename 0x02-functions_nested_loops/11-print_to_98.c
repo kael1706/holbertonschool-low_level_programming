f3e33e8fc98228ab250ce561e8a798a889f1189c
kael <cdcp-1795@hotmail.com>
2019-09-24 22:47:06 +0000

    fix answer 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-	for (n=n ; n < 98 ; n++)
+	for (n = n ; n < 98 ; n++)
 	{
 	printf("%i, ", n);
 	}
@@ -17,7 +17,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-	for (n=n ; n > 98 ; n--)
+	for (n = n ; n > 98 ; n--)
 	{
 	printf("%i, ", n);
 	}
