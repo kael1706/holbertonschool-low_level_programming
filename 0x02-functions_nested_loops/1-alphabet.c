70e777fb2aaad559a2927d38815041dded27e08f
kael <cdcp-1795@hotmail.com>
2019-09-24 05:31:18 +0000

    answer 0, 1, 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* print_alphabet - print alphabet
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_alphabet_x10 - print alphabet x10
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+	int nLines = 0;
+	while (nLines < 10)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	nLines++;
+														}
+}
