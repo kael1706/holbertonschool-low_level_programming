2116719e5732f818c3f1dcd10ade7dc276d25bbd
kael <cdcp-1795@hotmail.com>
2019-09-26 21:46:26 +0000

    fix answer 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,27 @@
 #include "holberton.h"
 /**
-  * print_square - print square of n*n
-  * @n: distance
-  */
+* print_square - print square of n*n
+* @n: distance
+*/
 void print_square(int n)
 {
-	char square[n][n];
-	int i, j;
-
-	for (i = 0; i < n; ++i)
+	if (n == 0 || n < 0)
 	{
-	for (j = 0; j < n ; ++j)
-	{
-	square[i][j] = '*';
-	putchar(square[i][j]);
+		_putchar('\n');
 	}
-	printf("\n");
+	else
+	{
+		char square[n][n];
+		int i, j;
+
+		for (i = 0; i < n; ++i)
+		{
+			for (j = 0; j < n ; ++j)
+			{
+				square[i][j] = '*';
+				_putchar(square[i][j]);
+			}
+			_putchar('\n');
+		}
 	}
 }
