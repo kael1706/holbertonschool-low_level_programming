5cc6f5d5a2cd8036106a766e385c6ecd57d91884
kael <cdcp-1795@hotmail.com>
2019-09-26 20:27:30 +0000

    answer 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+*print_diagonal - print diagonal line with n distance
+*@n: distance
+*
+*/
+void print_diagonal(int n)
+{
+	int i = 0;
+	char diagonal = '\\';
+	char spaces = ' ';
+
+	if (n == 0 || n < 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	while (i < n)
+	{
+	int j = 0;
+
+	while (j < i)
+	{
+	putchar(spaces);
+	j++;
+	}
+	_putchar(diagonal);
+	_putchar('\n');
+	i++;
+	}
+	}
+}
