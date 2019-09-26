5e82b082e205975f2db5a22cf5607c703baa84be
kael <cdcp-1795@hotmail.com>
2019-09-26 18:26:41 +0000

    fix answer 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,8 +5,6 @@
 */
 void more_numbers(void)
 {
-	char dig1, dig2;
-	int nLines = 0;
 	int i, j;
 	char hola[21] = "01234567891011121314";
 
@@ -14,9 +12,9 @@ void more_numbers(void)
 
 	while (j < 10)
 	{
-		for (int i = 0; i < 21; i++)
+		for (i = 0; i < 21; i++)
 		{
-			putchar(hola[i]);
+			_putchar(hola[i]);
 		}
 		j++;
 		_putchar('\n');
