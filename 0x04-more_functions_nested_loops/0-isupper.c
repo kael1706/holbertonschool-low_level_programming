1a7d9cfa1ff724c03361d1379411d159758f8b1e
kael <cdcp-1795@hotmail.com>
2019-09-26 13:59:32 +0000

    answer 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+*supper - detect if is uppercase
+*@c: the character for scanning
+*Return: 0 -> lowercase, 1 -> uppercase
+*
+*/
+   int _isupper(int c){
+	   int resp;
+
+	   if (c > 64 && c < 91)
+	   {
+		   resp = 1;
+	   }
+	   else
+	   {
+		   resp = 0;
+	   }
+	   return (resp);
+   }
