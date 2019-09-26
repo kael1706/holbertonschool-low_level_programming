20d5514b0ac28065185108ae39fe72b5bfba0f5b
kael <cdcp-1795@hotmail.com>
2019-09-26 14:50:07 +0000

    answer 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+*mul - multiplication operation
+*  @a: value to multiply
+*  @b: value to multiply
+*
+*  Return: an multiplication between @a and @b
+*
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
