6166ccd58561942eeef5162fa2200116aec77065
kael <cdcp-1795@hotmail.com>
2019-10-01 21:45:25 +0000

    answer 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _puts - print string with array
+* @str: string
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i[str] != '\0')
+	{
+	_putchar (str[i]);
+	}
+	_putchar ('\n');
+}
