40fe6545154f08461f97e559913a363e16031a68
kael <cdcp-1795@hotmail.com>
2019-10-03 22:16:50 +0000

    answer 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
 
+/**
+* _strcmp - compare two strings.
+* @s1: string
+* @s2: string
+* Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+	s1++;
+	s2++;
+	}
+
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+}
