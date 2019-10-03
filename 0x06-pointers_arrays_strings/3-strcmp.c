afc1319fbb6037ab1e40f10a517219f110fa38ef
kael <cdcp-1795@hotmail.com>
2019-10-03 21:32:14 +0000

    answer 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* _strncpy - copy a string.
+* @dest: array 1
+* @src: array 2
+* @n: copy limit (bytes)
+*
+* Description: work exactly like strncpy
+* Return: pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int pos = 0;
+
+	while (src[pos] != 0 && pos < n)
+	{
+	dest[pos] = src[pos];
+	pos++;
+	}
+
+	while (pos < n)
+	{
+	dest[pos] = '\0';
+	pos++;
+	}
+
+	return (dest);
+}
