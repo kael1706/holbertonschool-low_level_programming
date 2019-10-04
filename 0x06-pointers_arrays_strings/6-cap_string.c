274ab70b6625b15e484734bdc5dfddfefd0dc468
kael <cdcp-1795@hotmail.com>
2019-10-04 03:04:03 +0000

    fix answer 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,61 +1,31 @@
 #include "holberton.h"
 
 /**
-*status - detect status
-*@s: array
-*@changue: int
-*
-*Return: int
+* *cap_string - capitalizes the first letter of
+* words
+* @s: string
+* Return: string capitalized
 */
-int status(char *s, int changue, int j)
-{
-	if (s[j] == ' ')
-		changue = 0;
-	else if (s[j] == '\t')
-		changue = 0;
-	else if (s[j] == '\n')
-		changue = 0;
-	else if (s[j] == ',')
-		changue = 0;
-	else if (s[j] == ';')
-		changue = 0;
-	else if (s[j] == '.')
-		changue = 0;
-	else if (s[j] == '!')
-		changue = 0;
-	else if (s[j] == '?')
-		changue = 0;
-	else if (s[j] == '"')
-		changue = 0;
-	else if (s[j] == '(')
-		changue = 0;
-	else if (s[j] == ')')
-		changue = 0;
-	else if (s[j] == '{')
-		changue = 0;
-	else if (s[j] == '}')
-		changue = 0;
-	else
-		changue = changue + 1;
-	return (changue);
-}
-
-/**
-  * *cap_string - capitalizes the first letter of
-  * words
-  * @s: string
-  * Return: string capitalized
-  */
 char *cap_string(char *s)
 {
-	int j;
+	int j, i;
 	int changue = 0;
 
 	for (j = 0; s[j] != 0; j++)
 	{
-		status(s, changue, j);
-		if (changue == 1 && (s[j] >= 'a' && s[j] <= 'z'))
-			s[j] = s[j] - 32;
+	if ((s[j] == ' ') || (s[j] == '\t') ||
+		(s[j] == '\n') || (s[j] == ',') ||
+		(s[j] == ';') || (s[j] == '.') ||
+		(s[j] == '!') || (s[j] == '?') ||
+		(s[j] == '"') || (s[j] == '(') ||
+		(s[j] == ')')  || (s[j] == '{') ||
+		(s[j] == '}'))
+		changue = 0;
+	else
+		changue = changue + 1;
+	if (changue == 1 && (s[j] >= 'a' && s[j] <= 'z'))
+		s[j] = s[j] - 32;
+		printf("%c\n", s[j]);
 	}
 	return (s);
 }
