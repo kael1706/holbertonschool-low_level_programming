0af120146f44539618226bc86af42506ef4137c3
kael <cdcp-1795@hotmail.com>
2019-10-04 00:08:31 +0000

    fix answer 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,13 @@
 #include "holberton.h"
 
 /**
-* *cap_string - capitalizes the first letter of
-* words
-* @s: string
-* Return: string capitalized
+*status - detect status
+*@s: array
+*@changue: int
+*
+*Return: int
 */
-char *cap_string(char *s)
-{
-	int j;
-	int changue = 0;
-
-	for (j = 0; s[j] != 0; j++)
-	{
-		status();
-		if (changue == 1 && (s[j] >= 'a' && s[j] <= 'z'))
-			s[j] = s[j] - 32;
-	}
-	return (s);
-}
-void status(void)
+void status(char *s, int changue)
 {
 	if (s[j] == ' ')
 		changue = 0;
@@ -49,4 +37,25 @@ void status(void)
 		changue = 0;
 	else
 		changue = changue + 1;
+	return (changue);
+}
+
+/**
+  * *cap_string - capitalizes the first letter of
+  * words
+  * @s: string
+  * Return: string capitalized
+  */
+char *cap_string(char *s)
+{
+	int j;
+	int changue = 0;
+
+	for (j = 0; s[j] != 0; j++)
+	{
+		status(s, changue);
+		if (changue == 1 && (s[j] >= 'a' && s[j] <= 'z'))
+			s[j] = s[j] - 32;
+	}
+	return (s);
 }
