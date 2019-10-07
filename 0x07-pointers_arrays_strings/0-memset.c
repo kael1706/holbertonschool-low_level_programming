7b947d3ef606396829cdd6777ad709726e4bf5f5
kael1706 <cdcp-1795@hotmail.com>
2019-10-07 14:28:23 -0500

    answer 0 0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+/**
+ * _memset - fill memory .
+ * @s: pointer
+ * @b: hexadecimal number
+ * @n: n bytes
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    while (n-- > 0)
+		*s++ = b;
+	return (s);
+}
