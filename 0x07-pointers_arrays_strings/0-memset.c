37959751e3cb2c09fb126cafad60c0dbe8c3922a
kael <cdcp-1795@hotmail.com>
2019-10-07 19:48:12 +0000

    fix answer 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
- /**
- * _memset - fill memory .
- * @s: pointer
- * @b: hexadecimal number
- * @n: n bytes
- * Return: pointer
- */
+/**
+* _memset - fill memory .
+* @s: pointer
+* @b: hexadecimal number
+* @n: n bytes
+* Return: pointer
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-    while (n-- > 0)
+	while (n-- > 0)
 		*s++ = b;
 	return (s);
 }
