fbdf913f62c92ec4d6086b9b6f4f343e5a62fe55
kael <cdcp-1795@hotmail.com>
2019-10-08 04:52:03 +0000

    answer 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,17 +9,17 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int r, m;
+int r, m;
 
-	for (m = 0 ; needle[m] != '\0'; m++)
-	{
-		for (r = 0 ; haystack[r] != '\0'; r++)
-		{
-			if (haystack[r] == needle[m])
-			{
-				return (haystack + r);
-			}
-		}
-	}
-	return (0);
+for (m = 0 ; needle[m] != '\0'; m++)
+{
+for (r = 0 ; haystack[r] != '\0'; r++)
+{
+if (haystack[r] == needle[m])
+{
+return (haystack + r);
+}
+}
+}
+return (0);
 }
