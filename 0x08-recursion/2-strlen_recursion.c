dfc8b045be117aada27cff0b1da122ae5e5b10fa
kael <cdcp-1795@hotmail.com>
2019-10-10 04:47:22 +0000

    answer 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*_strlen_recursion - know length
+*@s: char
+*
+*/
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+}
