d2813c980208d1ba995372df8ef6d2a68d92232e
kael <cdcp-1795@hotmail.com>
2019-10-13 04:13:29 +0000

    answer 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*_isdigit - detect if is uppercase
+*@c: the character for scanning
+*Return: 0 -> !digit, 1 -> digit
+*
+*/
+int _isdigit(int c)
+{
+	int resp;
+
+	if (c > 47 && c < 58)
+	{
+		resp = 1;
+	}
+	else
+	{
+		resp = 0;
+	}
+	return (resp);
+}
+
+/**
+* main - sum operation (only positive)
+* @argc: number of arguments
+* @argv: arguments
+* Return: sum result
+*/
+int main(int argc, char *argv[])
+{
+	unsigned int r = 0;
+	int i = 1;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		if (_isdigit(*argv[i]) == 1)
+		{
+			r = r + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+	printf("%d\n", r);
+	return (0);
+}
