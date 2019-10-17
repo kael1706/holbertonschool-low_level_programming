ba5e9999f4bfaf48a7265185c51acaf107b643db
kael <cdcp-1795@hotmail.com>
2019-10-17 20:43:01 +0000

    answer 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <limits.h>
+
+/**
+* malloc_checked - test malloc
+* @b: bytes
+* Return: 98 if fail
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *tmp;
+
+	tmp = malloc(b);
+	if (tmp == NULL)
+	{
+		free(tmp);
+		exit(98);
+	}
+	return (tmp);
+}
