ef02ddf20e4616c77f30ee0d232ffad4b995a78a
kael <cdcp-1795@hotmail.com>
2019-10-22 01:17:12 +0000

    start

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _FL_DOG_H_
+#define _FL_DOG_H_
+
+/**
+* struct dog - prototype of object dog
+* @name: name (string)
+* @age: age (float)
+* @owner: owner (string)
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif 
