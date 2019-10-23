cbd47bf2fea4edf3e155f4cef93b1b809a96283d
kael <cdcp-1795@hotmail.com>
2019-10-23 15:21:02 +0000

    start proyect 0x0F

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef _FL_function_pointers
+#define _FL_function_pointers
+void f(char *);
+void print_name(char *name, void (*f)(char *));
+void action(int);
+void array_iterator(int *array, size_t size, void (*action)(int));
+int cmp(int);
+int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+#endif
