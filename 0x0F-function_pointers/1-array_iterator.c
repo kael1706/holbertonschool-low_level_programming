b6a5ec836666d7b56722e42ebbe6e0efa1e1a127
kael <cdcp-1795@hotmail.com>
2019-10-23 19:19:45 +0000

    answer 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,14 @@
+/**
+*array_iterator - run a function with the elements of array
+*@array: array of numbers
+*@size: size of array
+*@action: void function (int)
+*/
 array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i = 0;
 
-	while(i<size)
+	while (i < size)
 	{
 		action(array[i]);
 		i++;
