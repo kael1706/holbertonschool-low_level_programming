7793724bea1cfc964516672f296073437a2d6891
kael <cdcp-1795@hotmail.com>
2019-11-07 23:38:47 +0000

    answer 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,27 +7,30 @@
 void print_binary(unsigned long int n)
 {
 	int nBits = 8;
-	unsigned long int m = 0;
-	unsigned long int h = 0;
-	int t = 0;
-
-	nBits = sizeof(unsigned long int) * nBits;
-
+	unsigned long int answer = 0, c_binary = 1;
+	nBits = (sizeof(n) * 8) - 1;
+	/**
+	  * 10000000000000000000000000000000
+	  * 2147483648
+	  */
+	c_binary = c_binary << nBits;
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	for (; nBits >= 0; nBits--)
+	/**
+	  * avoid left zeros, and take a step to the right.
+	  * and get the number of digits
+	  */
+	while (!(n & c_binary))
+		c_binary = c_binary >> 1;
+	for (; c_binary; c_binary = c_binary >> 1)
 	{
-		m = n >> nBits;
-		h = m & 1;
-		if (h)
-		{
-			t = 1;
+		answer = n & c_binary;
+		if (answer)
 			_putchar('1');
-		}
-		else if (t == 1)
+		else
 			_putchar('0');
 	}
 }
