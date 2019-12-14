476f24631d2a058425ca090bbf19c9daa02ab6cf
kael <cdcp-1795@hotmail.com>
2019-12-14 22:04:39 +0000

    fix a2

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 
+/**
+* dlistint_len - quantity of items in the list
+* @h: pointer pointing to the head of the list
+*
+* Return: quantity of items
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
-	int a = 0;
+	size_t a = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		a+=1;
+		a = a + 1;
 		h = h->next;
 	}
 
