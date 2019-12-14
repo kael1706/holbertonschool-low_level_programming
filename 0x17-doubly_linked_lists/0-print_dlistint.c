9404e14fcfafae00ed956d5d4681e029baa88b9f
kael <cdcp-1795@hotmail.com>
2019-12-14 21:50:28 +0000

    a0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,17 @@
 #include "lists.h"
+/**
+* print_dlistint - print the quantity of items and items
+* @h: pointer pointing to the head of the list
+*
+* Return: quantity of items
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nNodes = 0;
-	
-	if (h ==NULL)
+
+	if (h == NULL)
 		return (0);
-	while (h !=NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
