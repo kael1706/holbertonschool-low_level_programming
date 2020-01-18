d20aee9daac4757ba975d67f6ce0306a2c7700de
kael <cdcp-1795@hotmail.com>
2020-01-18 20:39:25 +0000

    update a3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			n = n->next;
 		}
 	}
-	n_n->next = NULL;
+	n_n->next = ht->array[idx_ht];
 	ht->array[idx_ht] = n_n;
 	return (1);
 }
