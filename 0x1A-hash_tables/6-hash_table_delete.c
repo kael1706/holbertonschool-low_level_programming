60eda36374c1f8b135fdc9ed8efebbf6de0d232e
kael <cdcp-1795@hotmail.com>
2020-01-19 20:48:48 +0000

    a6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -3,9 +3,30 @@
 /**
  * hash_table_delete - delete a hash table
  *
- * Return: void 
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned int idx_ht;/* index hash table*/
+	hash_node_t *n;/*node*/
+	hash_node_t *t_n; /*temporal node*/
 
+	if (ht->array != NULL)
+	{
+		for (idx_ht = 0; idx_ht < ht->size; idx_ht++)
+		{
+			n = ht->array[idx_ht];
+			while (n != NULL)
+			{
+				t_n = n;
+				free(t_n->key);
+				free(t_n->value);
+				n = n->next;
+				free(t_n);
+			}
+		}
+	}
+	free(ht->array);
+	if (ht)
+		free(ht);
 }
